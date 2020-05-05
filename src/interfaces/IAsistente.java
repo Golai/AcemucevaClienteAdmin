3f91c2612c802177eca0499fc5ea94f183a8be6c
Luis Correal Viveros <49031153+Golai@users.noreply.github.com>
2020-05-04 22:36:06 -0500

    inicio

---
+++ src/interfaces/IAsistente.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+import java.rmi.RemoteException;
+
+public interface IAsistente {
+	boolean registrarAEventos(int id_asistente) throws RemoteException;
+
+}
+
---
+++ src/interfaces/IMiembro.java
@@ -0,0 +1,25 @@
+package interfaces;
+
+import java.rmi.RemoteException;
+import java.sql.Date;
+
+import persistence.Miembro;
+
+public interface IMiembro {
+	Miembro searchMiembro(int cedula) throws RemoteException;
+	boolean updateMiembro(int cedula,
+						  int id_cargo, 
+						  String nombre,
+						  int celular,
+						  int semestre, 
+						  int puntos) throws RemoteException;
+	boolean delMiembro(int cedula)throws RemoteException;
+	boolean addEvento(int cedula,
+			 		  int id_cargo, 
+			 		  String nombre,
+			 		  int celular,
+			 		  int semestre, 
+			 		  int puntos)throws RemoteException;
+
+
+}
